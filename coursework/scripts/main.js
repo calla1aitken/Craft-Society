3d6552015f24b4c638c70609975252a79bb5db69
calla1aitken <146110368+calla1aitken@users.noreply.github.com>
2024-04-05 21:41:48 +0100

    Add files via upload

--- coursework/scripts/main.js
+++ coursework/scripts/main.js
@@ -5,17 +5,26 @@ function send_form(submittedId, submittedText){
 
 var counter = 0;
 function login(button){
-    if(counter % 2 == 0){ //If counter is even, the user is logged in
+    var email = document.forms["login-form"]["login-email"].value;
+    var password = document.forms["login-form"]["password"].value;
+    document.forms["login-form"]["login-email"].value = "";
+    document.forms["login-form"]["password"].value = "";
+
+    if(counter % 2 == 0 && email == "" || counter % 2 == 0 && password == ""){ //If counter is even, the user is logged in
+        alert('You must enter your details');
+        
+    } else if (counter % 2 == 0){
         alert('You have been logged in!');
         var button = document.getElementById(button);
         button.innerHTML = "Log Out"; //Text changes to reflect whether use is logged in or out
+        counter += 1;
     }else { //If counter is odd, user is logged out
         alert('You have been logged out!');
         var button = document.getElementById(button);
         button.innerHTML = "Login"; //Text changes to reflect whether use is logged in or out
+        counter += 1;
     }
-    counter += 1;
-
+    
 }
 
 function imgGallery() {
@@ -29,6 +38,7 @@ function imgGallery() {
     
         function highlight(event){
             event.target.style.border = "solid #8f4218 10px";
+            event.target.style.cursor ="pointer"
         }
     
         function delight(event){
@@ -38,3 +48,23 @@ function imgGallery() {
 }
 //Calls the image gallery function
 imgGallery();
+
+function submit_event() {
+    var title = document.forms["events"]["event-title"].value;
+    var date = document.forms["events"]["event-date"].value;
+    document.forms["events"]["event-title"].value = "";
+    document.forms["events"]["event-date"].value = "";
+    document.forms["events"]["event-desc"].value = "";
+    if (title == "") {
+        alert("An event must have a title!");
+    }
+    else {
+        if (date.length == 0) {
+            alert("An event must have a date!");
+        }
+        else {
+            alert("Event submitted! The society will contact you soon!");
+        }
+    }
+}
+    
